4f520f70ac64859098ad938f6d3d317e16282069
noahstinson4 <stinson.noah4@gmail.com>
2026-04-25 14:58:13 -0500

    Delete card.tsx

--- card.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
-export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) { return <div className={cn("rounded-lg border bg-white text-slate-950 shadow-sm", className)} {...props} />; }
-export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) { return <div className={cn("p-6", className)} {...props} />; }
